88788bdb8f541b9f8849befa613949479a71b2ff
Ira Horecka <ira89@icloud.com>
2019-12-07 00:52:40 -0800

    Add StationKey and weather feature

--- LiveBartDataAcquisition.py
+++ LiveBartDataAcquisition.py
@@ -7,7 +7,9 @@ storage.
 
 import os
 import pandas as pd
+import pyowm
 from pybart.api import BART
+from StationKey import all_stations
 
 class DataFile:
     def __init__(self, csv_name):
@@ -40,8 +42,14 @@ class BartAPI:
         if not isinstance(first_departure_dict, dict):
             raise TypeError('Please input datatype as dictionary.')
         for station, minute_departure in first_departure_dict.items():
-            if minute_departure == 'Leaving':
-                print(station, 'is leaving!')
+            detail = minute_departure['detail']
+            if minute_departure['time'][0] == 'Leaving':
+                if station == 'SFIA':
+                    weather = WeatherAPI("Millbrae, California")    
+                else:
+                    weather = WeatherAPI(f"{detail['city']}, California")
+                print(station, 'is leaving with', minute_departure['time'][1], 'second(s) delay.')
+                print('Weather :', weather.atmosphere_weather())
 
     def fetch_first_departures(self):
         stn_abbr_dict = {}
@@ -50,18 +58,20 @@ class BartAPI:
             if not departure:
                 continue
             first_departure = self.sort_station_departures(departure)
-            stn_abbr_dict[station['abbr']] = first_departure
+            stn_abbr_dict[station['abbr']] = {'time': first_departure,
+                                              'detail': all_stations[station['abbr']]}
         return stn_abbr_dict
 
     def sort_station_departures(self, stn_departures):
         times_departure = []
-        for index,destination in enumerate(stn_departures):
+        for index, destination in enumerate(stn_departures):
             minute_departure = destination['estimate'][0]['minutes']
+            seconds_delay = destination['estimate'][0]['delay']
             minute_departure = 0 if minute_departure == 'Leaving' else minute_departure
-            times_departure.append((index, int(minute_departure)))
-        sorted_times_departure = sorted(times_departure, key = lambda x: x[1])
-        first_departure = sorted_times_departure[0][1]
-        first_departure = 'Leaving' if first_departure == 0 else first_departure
+            times_departure.append([int(minute_departure), int(seconds_delay)])
+        sorted_times_departure = sorted(times_departure, key = lambda x: x[0])
+        first_departure = sorted_times_departure[0]  # does not capture multiple 0min departures if present
+        first_departure[0] = 'Leaving' if first_departure[0] == 0 else first_departure[0]
         return first_departure
 
     def bart_stations(self):
@@ -69,8 +79,14 @@ class BartAPI:
 
 
 class WeatherAPI:
-    def __init__(self):
-        self.api_key = ''
+    def __init__(self, city_state_location):
+        self.owm = pyowm.OWM('984bf5177e2f7b06b6b4fd0a909e8206')
+        self.owm.get_API_key()
+        self.obs = self.owm.weather_at_place(city_state_location)  
+
+    def atmosphere_weather(self):
+        atmos = self.obs.get_weather().get_detailed_status()
+        return atmos
 
 
 def main():
